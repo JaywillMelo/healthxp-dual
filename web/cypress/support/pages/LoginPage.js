1d91c4bc06777c515d4726255dceda2a4461dc79
Ju <jmelo.15@hotmail.com>
2024-01-02 14:06:49 -0300

    meu primeiro commit

---
+++ web/cypress/support/pages/LoginPage.js
@@ -0,0 +1,42 @@
+
+import popup from './components/Popup'
+
+class LoginPage {
+
+    constructor() {
+        this.popup = popup
+    }
+
+    go() {
+        cy.visit('http://localhost:3000')
+    }
+
+    fill(user) {
+        if (user.email) {
+            cy.get('input[name=email]')
+                .clear() //.clear({force: true})
+                .type(user.email)
+        }
+
+        if (user.password) {
+            cy.get('#password')
+                .clear()
+                .type(user.password)
+        }
+    }
+
+    submit() {
+        //button[text()="Entrar"] - XPath
+
+        cy.contains('button', 'Entrar').click()
+    }
+
+    doLogin(user) {
+        this.go()
+        this.fill(user)
+        this.submit()
+    }
+
+}
+
+export default new LoginPage()
